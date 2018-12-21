d561b8b42f7195b9ee880857647f55a775e075d3
Steven Su <46003012+Saturn228@users.noreply.github.com>
2018-12-21 15:20:17 -0500

    Added movement relative to camera
    Need to add camera GameObject to script variables to function

---
+++ PlayerController.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+
+// Include the namespace required to use Unity UI
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	// Create public variables for player speed, and for the Text UI game objects
+	public float speed;
+	public Text countText;
+	public Text winText;
+    public float JumpForce;
+    public GameObject camera;
+
+	// Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
+	private Rigidbody rb;
+	private int count;
+    private bool hasjumped;
+
+	// At the start of the game..
+	void Start ()
+	{
+		// Assign the Rigidbody component to our private rb variable
+		rb = GetComponent<Rigidbody>();
+
+		// Set the count to zero 
+		count = 0;
+
+		// Run the SetCountText function to update the UI (see below)
+		SetCountText ();
+
+		// Set the text property of our Win Text UI to an empty string, making the 'You Win' (game over message) blank
+		winText.text = "";
+
+        // Set jump controlling bool to false
+        hasjumped = false;
+
+    }
+
+    // Each physics step..
+    void FixedUpdate ()
+	{
+		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+       
+        //camera forward and right vectors:
+        Vector3 forward = camera.transform.forward;
+        Vector3 right = camera.transform.right;
+
+        //project forward and right vectors on the horizontal plane (y = 0)
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+        
+        // Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
+        Vector3 movement = forward * moveVertical + right * moveHorizontal;
+
+        if (Input.GetKeyDown("space") & hasjumped == false)
+        {
+            movement.y += JumpForce;
+            hasjumped = true;
+        }
+
+        // Add a physical force to our Player rigidbody using our 'movement' Vector3 above, 
+        // multiplying it by 'speed' - our public player speed that appears in the inspector
+        rb.AddForce (movement * speed);
+	}
+
+	// When this game object intersects a collider with 'is trigger' checked, 
+	// store a reference to that collider in a variable named 'other'..
+	void OnTriggerEnter(Collider other) 
+	{
+		// ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			// Make the other game object (the pick up) inactive, to make it disappear
+			other.gameObject.SetActive (false);
+
+			// Add one to the score variable 'count'
+			count = count + 1;
+
+			// Run the 'SetCountText()' function (see below)
+			SetCountText ();
+		}
+
+        // ..and if the game object we intersect has the tag 'Jump Pad' assigned to it..
+        if (other.gameObject.CompareTag("Jump Pad"))
+        {
+
+            // Create movement vector
+            Vector3 movement = new Vector3(0.0f, 1*JumpForce, 0.0f);
+
+            // Apply movement vector to player object
+            rb.AddForce(movement*speed);
+            
+        }
+
+        
+
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        // if other game object is ground
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            // Player has landed and may jump again
+            hasjumped = false;
+
+        }
+
+
+
+    }
+
+
+
+	// Create a standalone function that can update the 'countText' UI and check if the required amount to win has been achieved
+	void SetCountText()
+	{
+		// Update the text field of our 'countText' variable
+		countText.text = "Count: " + count.ToString ();
+
+		// Check if our 'count' is equal to or exceeded 12
+		if (count >= 12) 
+		{
+			// Set the text value of our 'winText'
+			winText.text = "You Win!";
+		}
+	}
+
+    void Update()
+    {
+        
+       
+
+
+
+    }
+
+
+}
