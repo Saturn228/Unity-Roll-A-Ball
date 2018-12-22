531202e049c6bb08572fc743c467f2e59e0981b9
Steven Su <46003012+Saturn228@users.noreply.github.com>
2018-12-21 22:10:11 -0500

    Added RotateAround Script
    Script that has objects rotate around player at fixed radius

---
+++ Assets/_Completed-Game/Scripts/RotateAround.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+
+    // Public variables
+    public GameObject player;
+    public float speed;
+
+    // Private variables
+    private Rigidbody rb;
+    private float radius;
+    private float offset;
+
+    // Use this for initialization
+    void Start () {
+
+        // Get player rigidbody component
+	     rb = player.GetComponent<Rigidbody>();
+
+        // Set orbiting radius of object
+         radius = 2.0f;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        // Find distance between object and player
+        offset = (transform.position - player.transform.position).magnitude;
+        
+        // Check if distance is at target radius
+        if (offset != radius)
+        {
+            
+            // The step size is equal to difference between ideal and actual distances times speed times frame time.
+            float step = (offset - radius) * speed * Time.deltaTime;
+            
+            // Move our position a step closer to the target.
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+            
+        }
+        
+        // Spin the object around the player object
+        transform.RotateAround(player.transform.position, Vector3.up, (20 + 10*rb.velocity.magnitude) * Time.deltaTime);
+        
+    }
+}
