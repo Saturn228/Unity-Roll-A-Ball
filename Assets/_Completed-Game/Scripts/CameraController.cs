9a0820179a605eb6312554a30a35ee57f7fbaaa2
Steven Su <46003012+Saturn228@users.noreply.github.com>
2018-12-23 16:35:19 -0500

    Camera Rotation and Scroll Fix
    Made mouse control camera rotation and cleaned up scroll functionality. 
    Added camera distance correction.
    Cleaned up variable declarations.

--- Assets/_Completed-Game/Scripts/CameraController.cs
+++ Assets/_Completed-Game/Scripts/CameraController.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	// store a public reference to the Player game object, so we can refer to it's Transform
+    /* Public Variables */
+
+	// Player game object, so we can refer to it's Transform
 	public GameObject player;
 
     // Store speed of camera movement
@@ -12,15 +14,21 @@ public class CameraController : MonoBehaviour {
     // Store rotation angle
     public float rotateAngle;
 
+    /* Private Variables */
+
     // Store a Vector3 offset from the player (a distance to place the camera from the player at all times)
     private Vector3 offset;
+
+    // Store desired camera offset distance from player
+    private float radius;
 
     // At the start of the game..
     void Start ()
 	{
-		// Create an offset by subtracting the Camera's position from the player's position
+		// Create an offset by subtracting the Camera's position from the player's position and set that as desired starting radius
 		offset = transform.position - player.transform.position;
-        
+        radius = offset.magnitude;
+
         // Angle of rotation convert to radians
         //rotateAngle = rotateAngle*Mathf.PI / 180;
 	}
@@ -29,22 +37,33 @@ public class CameraController : MonoBehaviour {
     void LateUpdate()
     {
         
-        // Zoom in
-        if (offset.y > 5 && Input.mouseScrollDelta.y > 0)
+        // Zoom in or out
+        if ((offset.y > 5 && Input.mouseScrollDelta.y > 0) || (offset.y < 10 && -Input.mouseScrollDelta.y > 0))
         {
-            offset.y += -Input.mouseScrollDelta.y * speed;
-            offset.z += Input.mouseScrollDelta.y * speed;
+            // Create scroll delta vector
+            Vector3 scrolldata = new Vector3(0, -Input.mouseScrollDelta.y * speed, Input.mouseScrollDelta.y * speed);
+
+            // Update camera offset
+            offset += scrolldata;
+
+            // Update desired radius value
+            if (Input.mouseScrollDelta.y > 0)
+                radius += -scrolldata.magnitude;    
+            else
+                radius += scrolldata.magnitude;
 
         }
         
-        // Zoom out
-        if (offset.y < 10 && -Input.mouseScrollDelta.y > 0)
+        // Rotate Camera
+        if (Input.GetAxis("Mouse X") != 0)
         {
-            offset.y += -Input.mouseScrollDelta.y * speed;
-            offset.z += Input.mouseScrollDelta.y * speed;
-
+            // Rotate camera around player
+            offset = Quaternion.AngleAxis(-rotateAngle * Input.GetAxis("Mouse X") * speed, Vector3.up) * offset;
+            
         }
-
+        
+        // Camera rotation using keyboard
+        /*
         // Rotate right
         if (Input.GetKeyDown("e"))
         {
@@ -61,12 +80,27 @@ public class CameraController : MonoBehaviour {
             offset = Quaternion.AngleAxis(rotateAngle, Vector3.up) * offset;
             
         }
-    
+    */
+
         // Set the position of the Camera (the game object this script is attached to)
         // to the player's position, plus the offset amount
         transform.position = player.transform.position + offset;
 
+        // Check if distance is at target radius
+        if (offset.magnitude != radius)
+        {
+
+            // The step size is equal to difference between ideal and actual distances times speed times frame time.
+            float step = (offset.magnitude - radius) * speed * Time.deltaTime;
+
+            // Move our position a step closer to the target.
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+
+        }
+
         // Look at player object
         transform.LookAt(player.transform);
+
+        
     }
 }
