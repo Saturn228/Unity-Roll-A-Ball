88f96c2c91a7c90f868c467babc05f228aafabdd
Steven Su <46003012+Saturn228@users.noreply.github.com>
2019-01-04 16:03:10 -0500

    Add zoom reset
    Added zoom reset and decoupled zoom functionality into separate function. Added new variables ScrollData to contain scroll reduce polling of scroll information and ScrollVector to contain scroll vector transform for offset

--- Assets/_Completed-Game/Scripts/CameraController.cs
+++ Assets/_Completed-Game/Scripts/CameraController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class CameraController : MonoBehaviour {
 
@@ -13,7 +14,7 @@ public class CameraController : MonoBehaviour {
 
     // Store rotation angle
     public float rotateAngle;
-
+    
     /* Private Variables */
 
     // Store a Vector3 offset from the player (a distance to place the camera from the player at all times)
@@ -22,12 +23,24 @@ public class CameraController : MonoBehaviour {
     // Store desired camera offset distance from player
     private float radius;
 
+    // Store default camera zoom distance
+    private float CameraDefaultZoom;
+
+    // Store Input.mouseScrollDelta.y data
+    private float ScrollData;
+
     // At the start of the game..
     void Start ()
 	{
 		// Create an offset by subtracting the Camera's position from the player's position and set that as desired starting radius
 		offset = transform.position - player.transform.position;
+
+        // Initialize radius value and default zoom distance
         radius = offset.magnitude;
+        CameraDefaultZoom = radius;
+
+        // Initialize to no scroll motion detected
+        ScrollData = 0;
 
         // Angle of rotation convert to radians
         //rotateAngle = rotateAngle*Mathf.PI / 180;
@@ -36,21 +49,14 @@ public class CameraController : MonoBehaviour {
     // After the standard 'Update()' loop runs, and just before each frame is rendered..
     void LateUpdate()
     {
-        
+        // Assign scroll activity to ScrollData
+        ScrollData = Input.mouseScrollDelta.y;
+
         // Zoom in or out
-        if ((offset.y > 5 && Input.mouseScrollDelta.y > 0) || (offset.y < 10 && -Input.mouseScrollDelta.y > 0))
+        if ((offset.y > 5 && ScrollData > 0) || (offset.y < 10 && -ScrollData > 0))
         {
-            // Create scroll delta vector
-            Vector3 scrolldata = new Vector3(0, -Input.mouseScrollDelta.y * speed, Input.mouseScrollDelta.y * speed);
-
-            // Update camera offset
-            offset += scrolldata;
-
-            // Update desired radius value
-            if (Input.mouseScrollDelta.y > 0)
-                radius += -scrolldata.magnitude;    
-            else
-                radius += scrolldata.magnitude;
+            // Apply zoom to camera
+            Zoom(ref offset, ref radius, ScrollData);
 
         }
         
@@ -61,7 +67,7 @@ public class CameraController : MonoBehaviour {
             offset = Quaternion.AngleAxis(-rotateAngle * Input.GetAxis("Mouse X") * speed, Vector3.up) * offset;
             
         }
-        
+
         // Camera rotation using keyboard
         /*
         // Rotate right
@@ -82,6 +88,20 @@ public class CameraController : MonoBehaviour {
         }
     */
 
+        // Reset camera to default zoom distance
+        if (Input.GetMouseButtonDown(2))
+        {
+            // Zoom mouse until default zoom distance is reached
+            while (Math.Abs(radius - CameraDefaultZoom) > 0.1)
+            {
+                
+                // Apply zoom to camera with step size moving towards default zoom distance
+                Zoom(ref offset, ref radius, (radius - CameraDefaultZoom) * speed * Time.deltaTime);
+                
+            }
+
+        }
+
         // Set the position of the Camera (the game object this script is attached to)
         // to the player's position, plus the offset amount
         transform.position = player.transform.position + offset;
@@ -97,10 +117,30 @@ public class CameraController : MonoBehaviour {
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
 
         }
-
+        
         // Look at player object
         transform.LookAt(player.transform);
 
+        // Reset ScrollData to default
+        ScrollData = 0;
+    }
+
+    // Apply zoom to camera
+    void Zoom(ref Vector3 offset, ref float radius, float ScrollData)
+    {
+        
+        // Create scroll delta vector
+        Vector3 scrollvector = new Vector3(0, -ScrollData * speed, ScrollData * speed);
+
+        // Update camera offset
+        offset += scrollvector;
+
+        // Update desired radius value
+        if (ScrollData > 0)
+            radius += -scrollvector.magnitude;
+        else
+            radius += scrollvector.magnitude;
         
     }
+    
 }
