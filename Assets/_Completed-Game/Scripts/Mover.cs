2d6617499955b77abb2f77f83432a8f7df2438be
Steven Su <46003012+Saturn228@users.noreply.github.com>
2019-01-06 13:14:15 -0500

    Fixed non movement bug
    Changed handling of destination behavior to fix non movement bug when step size was small.

--- Assets/_Completed-Game/Scripts/Mover.cs
+++ Assets/_Completed-Game/Scripts/Mover.cs
@@ -9,25 +9,31 @@ public class Mover : MonoBehaviour {
     // Store vector to travel along
     public Vector3 travelVector;
 
-    // Store speed of wall movement
+    // Store speed of object movement
     public float speed;
 
     /* Private Variables */
 
-    // Store the start position of the wall
+    // Store the start position of object
     private Vector3 startPosition;
 
     // Store step size of movement vector
     private float step;
-    
+
+    // Store destination of object
+    private Vector3 destination;
+
     // Use this for initialization
     void Start () {
 
-        // Set starting position of wall
+        // Set starting position of object
         startPosition = transform.position;
 
+        // Set initial destination of object
+        destination = startPosition + travelVector;
+
         // The step size is equal to difference between current position and final position distances times speed times frame time.
-        step = (travelVector.magnitude + startPosition.magnitude - transform.position.magnitude) * speed * Time.deltaTime;
+        step = (destination.magnitude - transform.position.magnitude) * speed * Time.deltaTime;
         
     }
 
@@ -35,15 +41,26 @@ public class Mover : MonoBehaviour {
     void Update () {
 
         // Move our position a step closer to the target.
-        transform.position = Vector3.MoveTowards(transform.position, startPosition + travelVector, step);
+        transform.position = Vector3.MoveTowards(transform.position, destination, step);
         
-        // Has wall reached destination or starting position
-        if (transform.position == startPosition + travelVector || transform.position == startPosition)
+        // Has wall reached destination
+        if (transform.position == destination)
         {
-            // Change direction of travel
-            step *= -1;
-            
+            // Check if destination is the starting position of object
+            if(destination == startPosition)
+            {
+                // Change destination of object to travel position
+                destination += travelVector;
+                
+            }
+            else
+            {
+                // Change destination of object to start position
+                destination -= travelVector;
+                
+            }
+
         }
-        
+
     }
 }
