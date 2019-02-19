c9b78f7e8f9ab94eaf42f54b1fcc225a42fed532
Steven Su <46003012+Saturn228@users.noreply.github.com>
2019-02-18 19:31:27 -0500

    File Controller Script
    Added file controller for saving best times. Basis of time trial functionality working. Removed file control functionality from UI controller to File Controller

---
+++ Assets/_Completed-Game/Scripts/FileController.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class FileController : MonoBehaviour {
+
+    // Player game object
+    public GameObject Canvas;
+
+    /* Private Variables */
+
+    // UI controller script
+    UIController UIControllerScript;
+
+    // Path to best time text file and variable to store name of levels
+    string FileName;
+    string DirectoryPath;
+    string CurrentName;
+
+    // Current Level
+    Scene CurrentScene;
+
+    // Use this for initialization
+    void Start () {
+
+        // Retrieve player controller script
+        UIControllerScript = Canvas.GetComponent<UIController>();
+
+        // Set path to best time text file
+        DirectoryPath = "Assets/Resources/";
+        FileName = "besttime.txt";
+
+        // Get current level
+        CurrentScene = SceneManager.GetActiveScene();
+
+        // Set the text for best time for the level to no time
+        UIControllerScript.bestTimeText.text = "Best Time: --.-----";
+        
+        // Try to open best time text file
+        try
+        {
+            using (StreamReader reader = new StreamReader(DirectoryPath + FileName))
+            {
+
+                // Find the current level within best time text file
+                while ((CurrentName = reader.ReadLine()) != null)
+                {
+
+                    // If level is found within best time text file
+                    if (isSameScene())
+                    {
+                        // Display best time for level
+                        UIControllerScript.bestTimeText.text = "Best " + reader.ReadLine();
+                        break;
+                    }
+                }
+            }
+        }
+
+        // File does not exist so best time text stays at no time
+        catch (FileNotFoundException)
+        {
+            // Do nothing
+        }
+
+        // Directory does not exist so 
+        catch (DirectoryNotFoundException)
+        {
+            Directory.CreateDirectory(DirectoryPath);
+        }
+    }
+    
+    public void SaveBestTime()
+    {
+        // Try to open best time text file
+        try
+        {
+            using (StreamReader reader = new StreamReader(DirectoryPath + FileName))
+            {
+
+                // Create variable to store contents of best time text file
+                string BestTimeContentsBefore = "";
+
+                // Find the current level within best time text file
+                do
+                {
+                    // Read next line in file
+                    CurrentName = reader.ReadLine();
+
+                    // Add currently read line to best time contents
+                    BestTimeContentsBefore += CurrentName;
+
+                    // If level is found within best time text file
+                    if (isSameScene())
+                    {
+                        // Save previous best time for current level
+                        string pastbesttime = reader.ReadLine();
+
+                        // Convert time strings to doubles
+                        double pastTime, newTime;
+
+                        string[] temp1 = pastbesttime.Split(' ');
+                        string[] temp2 = UIControllerScript.timerText.text.Split(' ');
+
+                        double.TryParse(temp1[1], out pastTime);
+                        double.TryParse(temp2[1], out newTime);
+
+                        // Check if new best time was achieved
+                        if (pastTime > newTime)
+                        {
+                            // Save all content in best time text file after new time
+                            string BestTimeContentsAfter = reader.ReadToEnd();
+
+                            // Close reader to allow writing to best time text file
+                            reader.Close();
+
+                            // Write updated besttime.txt file
+                            StreamWriter writer = new StreamWriter(DirectoryPath + FileName);
+
+                            writer.WriteLine(BestTimeContentsBefore);
+                            writer.WriteLine(UIControllerScript.timerText.text);
+                            writer.WriteLine(BestTimeContentsAfter);
+                            writer.Close();
+                        }
+                        break;
+                    }
+                } while ((CurrentName = reader.ReadLine()) != null);
+
+                // If current level is not found
+                if (CurrentName == null)
+                {
+                    // Close read stream for best time text file
+                    reader.Close();
+
+                    // Write current time and level to the besttime.txt file
+                    StreamWriter writer = new StreamWriter(DirectoryPath + FileName);
+
+                    writer.WriteLine(BestTimeContentsBefore);
+                    writer.WriteLine("Level: " + CurrentScene.name);
+                    writer.WriteLine(UIControllerScript.timerText.text);
+                    writer.Close();
+                }
+            }
+        }
+
+        // File does not exist so new best time text file to be created
+        catch (FileNotFoundException)
+        {
+            StreamWriter writer = new StreamWriter(DirectoryPath + FileName);
+            
+            // Write name of level and best time to file
+            writer.WriteLine("Level: " + CurrentScene.name);
+            writer.WriteLine(UIControllerScript.timerText.text);
+            writer.Close();
+        }
+    }
+
+    // Check if current scene is same as selected scene
+    bool isSameScene()
+    {
+        return CurrentName == "Level: " + CurrentScene.name;        
+    }
+}
+
+
--- Assets/_Completed-Game/Scripts/UIController.cs
+++ Assets/_Completed-Game/Scripts/UIController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,14 +13,22 @@ public class UIController : MonoBehaviour {
     public Text countText;
     public Text winText;
     public Text playerMessageText;
+    public Text timerText;
+    public Text bestTimeText;
 
     // Player game object
     public GameObject player;
+    
+    // Player game object
+    public GameObject filescript;
 
     /* Private Variables */
 
     // Player controller script
     PlayerController playerControllerScript;
+
+    // File controller script
+    FileController fileControllerScript;
 
     // Use this for initialization
     void Start () {
@@ -31,7 +41,10 @@ public class UIController : MonoBehaviour {
 
         // Retrieve player controller script
         playerControllerScript = player.GetComponent<PlayerController>();
-        
+
+        // Retrieve player controller script
+        fileControllerScript = filescript.GetComponent<FileController>();
+
         // Tell player goals for level
         playerMessageText.text = "Collect all the gold pick ups and enter the red platform";
 
@@ -39,35 +52,35 @@ public class UIController : MonoBehaviour {
     
     // Update is called once per frame
     void Update () {
-
+        
         // Clear player message box
         if (Time.time > playerControllerScript.playerMessageTimer)
         {
             playerMessageText.text = "";
         }
 
+        // Update time displayed
+        if (!playerControllerScript.isFinished)
+        {
+            timerText.text = string.Format("Time: {0}", Time.timeSinceLevelLoad);
+        }
+
         // Tell player they have finished level
-        if (playerControllerScript.isFinished)
+        if (playerControllerScript.isFinished && winText.text != "You Win!")
         {
             // Set the text value of our 'winText'
             winText.text = "You Win!";
+
+            // Check if best time is surpassed
+            fileControllerScript.SaveBestTime();
         }
-	}
+    }
 
     // Create a standalone function that can update the 'countText' UI and check if the required amount to win has been achieved
     public void SetCountText(string text)
     {
         // Update the text field of our 'countText' variable
         countText.text = "Count: " + text;
-
-        /*
-		// Check if our 'count' is equal to or exceeded 12
-		if (count >= 4) 
-		{
-			// Set the text value of our 'winText'
-			winText.text = "You Win!";
-		}
-        */
     }
 
     // Create a standalone function that can update the 'countText' UI and check if the required amount to win has been achieved
